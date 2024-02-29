bd167714db02ed5d8c95d4a79050d93019853494
Konrad Michalik <konrad.michalik@xima.de>
2024-02-29 10:05:45 +0100

    feat: add further content

--- clockwork-ai/assets/script/lang.js
+++ clockwork-ai/assets/script/lang.js
@@ -2,17 +2,17 @@
 
 let arrLang = {
     "de-de": {
-        "intro-1": "Einfaches DIY Projekt um KI generierte",
+        "intro-1": `Einfaches <em>DIY</em> Projekt um <a href="https://chat.openai.com/" target="_blank">KI</a> generierte`,
         "poems": "Gedicht",
         "intro-2": "basierend auf der aktuellen Uhrzeit",
-        "intro-3": "auf einem E-Ink Display mit Raspberry Pi Zero zu rendern",
+        "intro-3": `auf einem <a href="https://www.waveshare.com/epaper" target="_blank">E-Ink Display</a> mit <a href="https://www.raspberrypi.com/products/raspberry-pi-zero/" target="_blank">Raspberry Pi Zero</a> zu rendern`,
         "installation": "Das gesamte Projekt Log sowie weitere Installationsanleitungen finden sich auf",
     },
     "en-gb": {
-        "intro-1": "Simple DIY project to render an AI generated",
+        "intro-1": `Simple <em>DIY</em> project to render an <a href="https://chat.openai.com/" target="_blank">AI</a> generated`,
         "poems": "poems",
         "intro-2": "based on the current time",
-        "intro-3": "on an e-ink display managed by a raspberry pi zero",
+        "intro-3": `on an <a href="https://www.waveshare.com/epaper" target="_blank">e-ink display</a> managed by a <a href="https://www.raspberrypi.com/products/raspberry-pi-zero/" target="_blank">raspberry pi zero</a>`,
         "installation": "See full project log and installation instructions on",
     }
   };
@@ -28,12 +28,12 @@ document.querySelector('.translate').addEventListener('click', function(event) {
     document.querySelectorAll('.lang').forEach(function(element) {
         var key = element.getAttribute('data-langkey');
         if(arrLang[targetLang] && arrLang[targetLang][key]) {
-            element.textContent = arrLang[targetLang][key];
+            element.innerHTML = arrLang[targetLang][key];
         }
     });
 
     event.target.setAttribute('data-target-langid', currentLang);
     event.target.setAttribute('data-current-langid', targetLang);
     event.target.setAttribute('data-target-langlabel', currentLabel);
-    event.target.textContent = targetLabel;
+    event.target.innerHTML = targetLabel;
 });
